19708c373bfa56f533cee3a2719369908ea941e5
annexus254 <njorogemainaanthony@gmail.com>
2021-11-06 18:21:03 +0300

    Create the HTML/CSS/JS skeleton for index.liquid

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const app               =   require("./server");
 const pokemonInsert     =   require("./models/pokemon_insert");
+const path              =   require('path');
+const {Liquid}          =   require("liquidjs");
+const engine            =   new Liquid({
+                                        root: path.resolve(__dirname , "views/"),
+                                        extname:".liquid"
+                                        });
 
 app.get("/storeseeddata",(req,resp)=>{
     const pokemonData   =   require("./models/pokemon");
@@ -7,4 +13,13 @@ app.get("/storeseeddata",(req,resp)=>{
         pokemonInsert.createPokemon(element , console.log);
     });
     resp.send("All Data Sent Successfully");
+});
+
+app.get('/', (req, resp) => {
+    let page = engine.renderFileSync("index" , {name:"Pokemon"});
+    resp.send(page);
+    });
+
+app.get("/views/custom.css" , (req , resp)=>{
+    resp.sendFile("custom.css" , { root: path.resolve(__dirname , "views/")});
 });
